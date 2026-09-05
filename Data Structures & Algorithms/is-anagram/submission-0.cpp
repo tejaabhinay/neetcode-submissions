98d6d1e79e8091dfca68689e9b0c7bbcbe5b5351
Teja Abhinay <pulavarthytejaabhinay@gmail.com>
2026-09-05 21:46:22 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq(256,0);
+        for(int i=0;i<s.length();i++){
+            freq[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            freq[t[i]]--;
+        }
+        for(int i=0;i<256;i++){
+            if(freq[i]!=0)
+                return false;
+        }
+        return true;
+    }
+};
